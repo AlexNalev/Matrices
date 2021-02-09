8c67e3a776aa123c168548144d23b85530e64a3b
Alex <navalugo.ale@gmail.com>
2021-02-09 16:42:15 -0600

    added the program to count even and odd numbers in array

---
+++ even_odd_numbers.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+void count_even_odd_numbers(vector<int> numbers){
+	int even_counter = 0, odd_counter = 0;
+	for(auto number : numbers){
+		if(number % 2 == 0){
+			even_counter ++;
+		}
+		else{
+			odd_counter ++;
+		}
+	}
+	cout << "You have:\n";
+	cout << "- " << even_counter " even numbers in the array\n";
+	cout << "- " << odd_counter " odd numbers in the array\n";
+}
+
+
+void ask_array_data(vector<int>& numbers){
+	int number_elements, auxiliary;
+	cout << "Number of elements? ";
+	cin >> number_elements;
+	
+	for(int i = 0; i < number_elements; i++){
+		cout << "Element " << i+1 << ": ";
+		cin >> auxiliary;
+		numbers.push_back(auxiliary);
+	}
+}
+
+int main(){
+	vector<int> n;
+	ask_array_data(n);
+	count_even_odd_numbers(n);
+
+	return 0;
+}
