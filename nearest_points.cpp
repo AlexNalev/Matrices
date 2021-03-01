033827be06f97d961b3fe67aa71e97fa5cd554c5
Alex <navalugo.ale@gmail.com>
2021-02-28 21:20:29 -0600

    Change nearest_points.cpp, change the implementation of the nearest_elements (queue -> vector)

--- nearest_points.cpp
+++ nearest_points.cpp
@@ -19,7 +19,7 @@
 */
 #include<iostream>
 #include<vector>
-#include<queue>
+#include<algorithm>
 
 using namespace std;
 
@@ -33,6 +33,7 @@ struct Coordinate{
 	}
 };
 
+
 bool compareDistance(Coordinate c1, Coordinate c2){
 	int distance1 = c1.x * c1.x + c1.y * c1.y;
 	int distance2 = c2.x * c2.x + c2.y * c2.y;
@@ -44,7 +45,8 @@ int main(){
 
 	//Store the coordinates
 	vector<Coordinate> coordinates{{3, 5}, {-1, 3}, {1, 1}, {-3, 1}, {-1, -1}, {3, -2}};
-	queue<Coordinate> nearest_elements;
+
+	vector<Coordinate> nearest_elements;
 
 	//Ask for the k nearest elements
 	cout << "Number of nearest points: ";
@@ -52,16 +54,16 @@ int main(){
 	
 	//Store the first k elements in the queue
 	for(int i = 0; i < k_points; i++){
-		nearest_elements.push(coordinates[i]);
+		nearest_elements.push_back(coordinates[i]);
 	}
 
-	//Sort the queue
-	while(!nearest_elements.empty()){
-		cout << nearest_elements.front().x << " , " << nearest_elements.front().y << endl;
-		nearest_elements.pop();
-	}
-	
+	//Sort nearest_elements
+	sort(nearest_elements.begin(), nearest_elements.end(), compareDistance);
 
+	//Show nearest_elements
+	for(auto i: nearest_elements){
+		cout << i.x << " , " << i.y << endl;
+	}
 
 	return 0;
 }
